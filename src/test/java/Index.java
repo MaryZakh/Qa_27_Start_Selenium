5e386a405a44965a31c114920b88aa453644a441
Maria Z <z0559882272@gmail.com>
2024-09-17 20:48:50 +0300

    sccSelectors(tag, class, id)

---
+++ src/test/java/Index.java
@@ -0,0 +1,50 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+
+public class Index {
+
+    WebDriver wd;
+
+
+    @BeforeClass
+    public void setUp() {
+        wd = new ChromeDriver();
+        wd.get("file:///C:/Users/marii/Downloads/21.index.html");
+    }
+
+
+    @Test
+    public void cssLocators() {
+        //by tagName
+
+        WebElement button = wd.findElement(By.tagName("button"));
+        WebElement button1 = wd.findElement(By.cssSelector("button"));
+
+        List<WebElement> list = wd.findElements(By.tagName("a"));
+        List<WebElement> list1 = wd.findElements(By.cssSelector("a"));
+
+
+        //by class
+
+        WebElement el = wd.findElement(By.className("container"));
+        WebElement el1 = wd.findElement(By.cssSelector(".container"));
+
+        List<WebElement> navlist = wd.findElements(By.className("nav-item"));
+        List<WebElement> navlist1 = wd.findElements(By.cssSelector(".nav-item"));
+
+        //by id
+        WebElement el2 = wd.findElement(By.id("nav"));
+        WebElement el3 = wd.findElement(By.cssSelector("#nav"));
+
+
+    }
+
+}
