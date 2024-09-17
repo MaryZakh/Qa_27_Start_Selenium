a39fc6bdf5d984d0550cc22a31969c97660c69dc
Maria Z <z0559882272@gmail.com>
2024-09-17 19:03:48 +0300

    added login tests

--- src/test/java/StartSelenuim.java
+++ src/test/java/StartSelenuim.java
@@ -7,11 +7,20 @@ public class StartSelenuim {
     WebDriver wd;
 
 
+    @Test
+    public void test() {
+        wd = new ChromeDriver();
+        //wd.get("https://telranedu.web.app/home");//without history
+        wd.navigate().to("https://telranedu.web.app/home");
+        wd.navigate().back();
+        wd.navigate().forward();
+        wd.navigate().refresh();
 
-   @Test
-   public void  test(){
-       wd = new ChromeDriver();
-       wd.get("https://telranedu.web.app/home");
-   }
+        wd.close();//only one tab if tab only one & close browser
+
+        wd.quit();// all tabs & close browser
+
+
+    }
 
 }
