190c44729efd4ad5d878fb3581ede7327fbb1417
Maria Z <z0559882272@gmail.com>
2024-09-20 09:50:06 +0300

    hw_1

--- src/test/java/PhoneBook.java
+++ src/test/java/PhoneBook.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -8,30 +10,52 @@ public class PhoneBook {
     WebDriver wd;
 
     @BeforeClass
-    public void setUp(){
-        wd=new ChromeDriver();
+    public void setUp() {
+        wd = new ChromeDriver();
         wd.get("https://telranedu.web.app/login");
     }
 
 
     @Test
-    public void selectors(){
+    public void selectors() {
 
         //by tagName
+        WebElement body = wd.findElement(By.tagName("body"));
+        WebElement body1 = wd.findElement(By.cssSelector("body"));
 
+        WebElement div = wd.findElement(By.tagName("div"));
+        WebElement div1 = wd.findElement(By.cssSelector("div"));
 
+        WebElement h1 = wd.findElement(By.tagName("h1"));
+        WebElement h1_1 = wd.findElement(By.cssSelector("h1"));
+
+        WebElement a = wd.findElement(By.tagName("a"));
+        WebElement a1 = wd.findElement(By.cssSelector("a"));
+
+        WebElement form = wd.findElement(By.tagName("form"));
+        WebElement form1 = wd.findElement(By.cssSelector("form"));
+
+        WebElement input = wd.findElement(By.tagName("input"));
+        WebElement input1 = wd.findElement(By.cssSelector("input"));
+
+        WebElement button = wd.findElement(By.tagName("button"));
+        WebElement button1 = wd.findElement(By.cssSelector("button"));
 
 
         //by className
+        WebElement el = wd.findElement(By.className("container"));
+        WebElement el1 = wd.findElement(By.cssSelector(".container"));
 
+        WebElement el2 = wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        WebElement el3 = wd.findElement(By.cssSelector(".navbar-component_nav__1X_4m"));
 
+        WebElement el4 = wd.findElement(By.className("active"));
+        WebElement el5 = wd.findElement(By.cssSelector(".active"));
 
 
         //by id
-
-
-
-
+        WebElement el6 = wd.findElement(By.id("root"));
+        WebElement el7 = wd.findElement(By.cssSelector("#root"));
 
     }
 }
